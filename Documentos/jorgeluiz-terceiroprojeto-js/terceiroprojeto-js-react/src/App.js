ae28f9800539f8f1c593a82d848fd38a0f348138
vai na web <vainawebcpx@gmail.com>
2020-01-17 12:23:56 -0300

    Criei uma header, Inseri Login, dei cor de fundo à header, falta colocar as imagens que faltam e fazê-las rodar, RJ 17 JAN 2020 - sexta-feira.

--- Documentos/jorgeluiz-terceiroprojeto-js/terceiroprojeto-js-react/src/App.js
+++ Documentos/jorgeluiz-terceiroprojeto-js/terceiroprojeto-js-react/src/App.js
@@ -1,47 +1,74 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import mainimage from "./assets/img01terceiroprojeto.jpg";
+// import secondimage from "./assets/imag02terceiroprojeto.jpg";
+// import thirdimage from "./assets/imag03terceiroprojeto.jpg";
+// import fourthimage from "./assets/imag04terceiroprojeto.jpg";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      imagesList:[
-      "https://cdn.pixabay.com/photo/2017/01/23/09/21/learn-2001838_960_720.jpg",
-      // 'assets/imag02terceiroprojeto',
-      // 'assets/imag03terceiroprojeto',
-      // 'assets/imag04terceiroprojeto'
-      ], 
+
+    this.state = {
+      user: {
+        email: '',
+        password1: '',
+        password2: '',
+        message: ''
+      },
+
+      imagesList: [
+        "https://cdn.pixabay.com/photo/2017/01/23/09/21/learn-2001838_960_720.jpg",
+        // "https://cdn.pixabay.com/photo/2016/03/09/09/17/computer-1245714_960_720.jpg",
+        
+        // 'assets/imag03terceiroprojeto',
+        // 'assets/imag04terceiroprojeto'
+      ],
       counter: 0
     }
-  }          
-    // let left=document.getElementById('left')
-    // let img=document.getElementById('img')
-    // let right=document.getElementById('right')
-    
-    left = (event) =>{
-      if (this.state.counter > 0) {
-        this.setState({counter: this.state.counter - 1})
-        }else {
-          this.setState({counter: 3})
-        }
-        document.getElementById('img').setAttribute('src', this.state.imagesList[this.state.counter]);
+  }
+  // let left=document.getElementById('left')
+  // let img=document.getElementById('img')
+  // let right=document.getElementById('right')
+
+  left = (event) => {
+    if (this.state.counter > 0) {
+      this.setState({ counter: this.state.counter - 1 })
+    } else {
+      this.setState({ counter: 3 })
     }
-    
-    right = (event) => {
-      if (this.state.counter < 3) {
-        this.setState({counter: this.state.counter + 1})        
-      } else {
-        this.setState({counter: 0})
-      }
-      document.getElementById('img').setAttribute('src', this.state.imagesList[this.state.counter]);
-    } 
-    
-  render(){
+    document.getElementById('img').setAttribute('src', this.state.imagesList[this.state.counter]);
+  }
+
+  right = (event) => {
+    if (this.state.counter < 3) {
+      this.setState({ counter: this.state.counter + 1 })
+    } else {
+      this.setState({ counter: 0 })
+    }
+    document.getElementById('img').setAttribute('src', this.state.imagesList[this.state.counter]);
+  }
+
+  render() {
     return (
       <div className="App">
-        <body class="body" id="segundoprojeto">
-          <header></header>
+        <body className="body" id="segundoprojeto">
+          <header className="header">
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                <p>EMAIL</p>
+                <input required autoFocus type='email' onchange={this.changeEmail}
+                  placeholder='name@email.com' />
+              </label>
+              <label>
+                <p>CODE</p>
+                <input required type='password' placeholder='#@ 123 abc !' onchange={this.changePassword} />
+              </label>
+              <button onClick={() => this.props.Timer}>Enter</button>
+              <p>{this.state.message}</p>
+            </form>
+          </header>
           <main class="main">
             <bottom onClick={this.left}>left</bottom>
             <img class='mainimag01' id="img" src="https://cdn.pixabay.com/photo/2017/01/23/09/21/learn-2001838_960_720.jpg" />
@@ -49,8 +76,7 @@ class App extends React.Component {
             {/* <img src="https://cdn.pixabay.com/photo/2016/03/27/18/54/technology-1283624_960_720.jpg" />
             <img src="assets/imag03terceiroprojeto" />
             <img src="assets/imag04terceiroprojeto" /> */}
-          </main>
-          <script type="text/javascript" src="main.js"></script>
+          </main>          
         </body>
       </div>
     );
